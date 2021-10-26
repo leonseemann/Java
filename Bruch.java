ddba5fc321ff8320abef06fc08be5615e6400c39
slowede <79045490+slowede@users.noreply.github.com>
2021-10-26 10:41:08 +0200

    java

---
+++ Bruch.java
@@ -0,0 +1,55 @@
+
+class Bruch {
+    private int zaehler;
+    private int nenner;
+
+    public Bruch() {
+        this(3);
+    }
+
+    public Bruch(int z) {
+        this(z, 1);
+    }
+
+    public Bruch(int z, int n) {
+        if (n <= 0) {
+            n = 1;
+        }
+
+        zaehler = z;
+        nenner = n;
+    }
+
+    public double ausgabeDezimal() {
+        double dezimal = (double) zaehler / (double) nenner;
+        return dezimal;
+    }
+
+    public void ausgabeBruch() {
+        System.out.println(zaehler + "/" + nenner);
+    }
+
+    public Bruch addition(Bruch b) {
+        Bruch ausgabeB = new Bruch(this.zaehler * b.nenner +  b.zaehler * this.nenner, this.nenner * b.nenner);
+
+        return ausgabeB;
+    }
+
+    public Bruch subtraktion(Bruch b) {
+        Bruch ausgabeB = new Bruch(this.zaehler * b.zaehler - b.nenner * this.nenner, this.nenner * b.nenner);
+
+        return ausgabeB;
+    }
+
+    public Bruch multiplikation(Bruch b) {
+        Bruch ausgabeB = new Bruch(this.zaehler * b.zaehler, this.nenner * b.nenner);
+
+        return ausgabeB;
+    }
+
+    public Bruch dividieren(Bruch b) {
+        Bruch ausgabeB = new Bruch(this.zaehler * b.nenner, this.nenner * b.zaehler);
+
+        return ausgabeB;
+    }
+}
