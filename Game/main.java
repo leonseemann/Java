3f966835ac366b5912b324e715ffc617b7124131
slowede <79045490+slowede@users.noreply.github.com>
2021-11-06 14:45:58 +0100

    game

---
+++ Game/main.java
@@ -0,0 +1,11 @@
+package Game;
+
+public class main extends zehntausend {
+    public static void main(String[] args) {
+        Player leon = new Player("Leon");
+
+        System.out.println(leon.getName());
+        System.out.println(leon.getPoints());
+        
+    }
+}
---
+++ Game/zehntausend.java
@@ -0,0 +1,31 @@
+package Game;
+
+public class zehntausend {
+    public static void fuenf(Player p) {
+        p.setPoints(p.getPoints() - 50);
+    }
+
+    public static void eins(Player p) {
+        p.setPoints(p.getPoints() - 100);
+    }
+
+    public static void dreimalzwei(Player p) {
+        p.setPoints(p.getPoints() - 200);
+    }
+
+    public static void dreimaldrei(Player p) {
+        p.setPoints(p.getPoints() - 300);
+    }
+
+    public static void dreimalvier(Player p) {
+        p.setPoints(p.getPoints() - 400);
+    }
+
+    public static void dreimalfuenf(Player p) {
+        p.setPoints(p.getPoints() - 500);
+    }
+
+    public static void dreimalsechs(Player p) {
+        p.setPoints(p.getPoints() - 600);
+    }
+}
