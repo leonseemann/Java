a69e2940410d757ba94b71776e0d1e2ead417c91
slowede <le.see@web.de>
2021-11-06 16:15:40 +0100

    game

--- Game/main.java
+++ Game/main.java
@@ -1,11 +1,11 @@
 package Game;
 
+import java.util.Scanner;
+
 public class main extends zehntausend {
     public static void main(String[] args) {
-        Player leon = new Player("Leon");
-
-        System.out.println(leon.getName());
-        System.out.println(leon.getPoints());
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Anzahl der Spieler");
+        int anzahl_spieler = scanner.nextInt();
     }
 }
