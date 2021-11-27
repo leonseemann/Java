a838e184c251cbd7f41c0dccb9889e0e0a7580ca
slowede <le.see@web.de>
2021-11-27 23:30:18 +0100

    test

--- Zeit/Start.java
+++ Zeit/Start.java
@@ -2,7 +2,8 @@ package Zeit;
 
 public class Start {
     public static void main(String[] args) {
-        myFrame Frame = new myFrame();
-        Frame.myFrame();
+        ZeitFormat12 zeit12 = new ZeitFormat12();
+
+        zeit12.print();
     }
 }
